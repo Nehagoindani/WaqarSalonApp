b99ec096bec12bfaf2504c64e21863a5c9a67947
Nehagoindani <73845534+Nehagoindani@users.noreply.github.com>
2022-06-15 00:57:05 +0500

    Update Main.js

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -104,3 +104,112 @@ function MainPage({ navigation }) {
 
 
 export default MainPage;
+
+
+const styles = StyleSheet.create({
+
+  scroll: {
+    backgroundColor: '#fcf7f0' // '#faf6e9'
+  },
+  searchbar: {
+    backgroundColor: 'black',
+    opacity: 0.2,
+    margin: 10,
+    padding: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 40
+  },
+  categryView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+
+  },
+  ctext: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    fontWeight: 'bold',
+    borderColor: '#d6994b',       //'#1a1a1a'
+    borderWidth: 1.25,
+    borderRadius: 8,
+    color: '#000',
+    backgroundColor: '#fcf7f0',   //'#fcf7f0' '#d6994b'
+    shadowOpacity: 0.4,
+    shadowRadius: 4.7,
+    elevation: 8,
+    width: 100,
+    textAlign: 'center',
+    margin: 10,
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+  },
+  disPanel: {
+    margin: 15,
+    width: 330,
+    height: 100,
+    backgroundColor: 'white',
+    borderColor: 'brown',
+    borderWidth: 20
+  },
+  disimg: {
+    position: 'relative',
+    left: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    width: 100,
+    height: 60,
+  },
+  specImg: {
+    width: 100,
+    height: 150,
+    margin: 5,
+    padding: 5,
+    borderRadius: 10,
+  },
+  specImg1: {
+    width: 330,
+    height: 151,
+    margin: 5,
+    padding: 5,
+    borderRadius: 10,
+  },
+  imgStyling: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+  },
+  imgStyling2: {
+    height: 50,
+    flex: 1,
+    margin: 5,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // shadowColor:'#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 4
+    // },
+    // shadowOpacity: 0.4,
+    // shadowRadius: 4.7,
+    // elevation: 8,
+  },
+  MapView: {
+    flex: 0.5,
+    margin: 10,
+  },
+  scrolling1: {
+    width: 400,
+    padding: 10,
+    marginBottom: 10,
+  },
+
+})
